07a02a729754eebbac401a2c7d603c9abf72c1be
xuejiuainiyo <3068440939@qq.com>
2023-09-03 13:43:54 +0800

    热门推荐的Tab切换

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -2,34 +2,40 @@
     <view class="viewport">
         <!-- 推荐封面图 -->
         <view class="cover">
-            <image
-                src="http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-05-20/84abb5b1-8344-49ae-afc1-9cb932f3d593.jpg"
-            >
-            </image>
+            <image :src="bannerPicture"> </image>
         </view>
         <!-- 推荐选项 -->
         <view class="tabs">
-            <text class="text active">抢先尝鲜</text>
-            <text class="text">新品预告</text>
+            <text
+                class="text"
+                :class="{ active: index === activeIndex }"
+                v-for="(item, index) in subTypes"
+                :key="item.id"
+                @tap="activeIndex = activeIndex = index"
+                >{{ item.title }}</text
+            >
         </view>
         <!-- 推荐列表 -->
-        <scroll-view scroll-y class="scroll-view">
+        <scroll-view
+            scroll-y
+            class="scroll-view"
+            v-for="(item, index) in subTypes"
+            :key="item.id"
+            v-show="activeIndex === index"
+        >
             <view class="goods">
                 <navigator
                     hover-class="none"
                     class="navigator"
-                    v-for="goods in 10"
-                    :key="goods"
-                    :url="`/pages/goods/goods?id=`"
+                    v-for="goods in item.goodsItems.items"
+                    :key="goods.id"
+                    :url="`/pages/goods/goods?id=${goods.id}`"
                 >
-                    <image
-                        class="thumb"
-                        src="https://yanxuan-item.nosdn.127.net/5e7864647286c7447eeee7f0025f8c11.png"
-                    ></image>
-                    <view class="name ellipsis">不含酒精，使用安心爽肤清洁湿巾</view>
+                    <image class="thumb" :src="goods.picture"></image>
+                    <view class="name ellipsis">{{ goods.name }}</view>
                     <view class="price">
                         <text class="symbol">¥</text>
-                        <text class="number">29.90</text>
+                        <text class="number">{{ goods.price }}</text>
                     </view>
                 </navigator>
             </view>
@@ -42,7 +48,7 @@
 import { ref } from 'vue'
 import { getHomeRecommendAPI } from '@/services/hot'
 import { onLoad } from '@dcloudio/uni-app'
-
+import { SubTypeItem } from '@/types/hot.d'
 const hotMap = [
     { type: '1', title: '特惠推荐', url: '/hot/preference' },
     { type: '2', title: '爆款推荐', url: '/hot/inVogue' },
@@ -58,10 +64,16 @@ const currUrlMap = hotMap.find((x) => x.type === query.type)
 uni.setNavigationBarTitle({
     title: currUrlMap!.title,
 })
+const bannerPicture = ref('')
+const subTypes = ref<SubTypeItem[]>([])
 const getHotData = async () => {
     const res = await getHomeRecommendAPI(currUrlMap!.url)
-    console.log(res)
+    console.log(res.result)
+    bannerPicture.value = res.result.bannerPicture
+    subTypes.value = res.result.subTypes
 }
+
+const activeIndex = ref(0)
 // 页面周期函数--监听页面加载
 onLoad(() => {
     getHotData()
